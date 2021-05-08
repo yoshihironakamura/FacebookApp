3cdf843091b514de979ba34bb25b74fbdde69fd1
yoshihironakamura <yoshironakamura491@gmail.com>
2021-05-08 12:14:48 +0000

    twitterアプリを作成

---
+++ twitter/app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "コメントを追加しました！"
+      else
+        render :new
+      end
+    end
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "コメントを編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"コメントを削除しました！"
+  end
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
