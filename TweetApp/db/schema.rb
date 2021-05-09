55264c5c7600a78c4e20904256fbb3d5a9b9c141
yoshihironakamura <yoshironakamura491@gmail.com>
2021-05-09 03:25:20 +0000

    postsテーブルからtitleカラムを削除

--- TweetApp/db/schema.rb
+++ TweetApp/db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_044035) do
+ActiveRecord::Schema.define(version: 2021_05_09_032010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "content"
   end
 
